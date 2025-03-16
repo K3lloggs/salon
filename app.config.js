723f6445fc91ee2049495173954727d149e49df6
K3lloggs <connorclosesln@gmail.com>
2025-03-15 22:48:39 -0400

    updated config

--- app.config.js
+++ app.config.js
@@ -4,6 +4,19 @@ export default {
   expo: {
     name: 'watch_salon',
     slug: 'salon',
+    ios: {
+      bundleIdentifier: 'com.itsabike.salon',
+      googleServicesFile: "./GoogleService-Info.plist",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    plugins: [
+      'expo-router',
+      "@react-native-firebase/app",
+      "@react-native-firebase/auth"
+    ],
+    owner: 'itsabike',
     extra: {
       FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
@@ -15,28 +28,6 @@ export default {
       eas: {
         projectId: '196d8bb9-d7b4-4c0d-91b7-00858fb9ff80'
       }
-    },
-    ios: {
-      bundleIdentifier: 'com.itsabike.salon',
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false
-      }
-    },
-    plugins: [
-      'expo-router',
-      [
-        '@react-native-firebase/app',
-        {
-          ios: {
-            googleServicesFile: "./GoogleService-Info.plist"
-          },
-          android: {
-            googleServicesFile: "./google-services.json"
-          }
-        }
-      ],
-      '@react-native-firebase/auth'
-    ],
-    owner: 'itsabike'
+    }
   }
 };
