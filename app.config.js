539bd3c580032ee919b2d3ba122747eb5c13f8d9
K3lloggs <connorclosesln@gmail.com>
2025-03-27 10:10:34 -0400

    1.0.4 b

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
     scheme: 'watchsalon', // <-- Required linking scheme for production builds
     icon: "./assets/images/shreve_circle.png",
     orientation: "portrait",
-    version: "1.0.3",  // Version number
+    version: "1.0.4",  // Version number
     extra: {
       FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
