44f16ecffa6c97c2e07470bf8afe6aa1172c262e
K3lloggs <connorclosesln@gmail.com>
2025-03-30 23:55:41 -0400

    1.1.0

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
     scheme: 'watchsalon', // <-- Required linking scheme for production builds
     icon: "./assets/images/scl_watch.jpg",
     orientation: "portrait",
-    version: "1.0.8",  // Version number
+    version: "1.1.0",  // Version number
     extra: {
       FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
@@ -16,8 +16,7 @@ export default {
       FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
       FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
       FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-      STRIPE_PUBLISHABLE_KEY:
-        "sk_test_51KOAMQDYuNaEOlQ2cMftzqDaJJVqIl6T8wLv0v84WJwfWx2JVojRulGtQf7nlEYSE0jsmVspizrMpBeY12BYlpWv004f15rpTd", // Replace with your actual key
+      STRIPE_PUBLISHABLE_KEY: "pk_live_51KOAMQDYuNaEOlQ2nqKvmYdL45mnKhQxEdOCJX5kgcpCmlHueINBYyPmggU0LHhPtUsUr2bKG8Iph5xSXsGlmi3h008ESuHAo0",
       eas: {
         projectId: '196d8bb9-d7b4-4c0d-91b7-00858fb9ff80'
       }
